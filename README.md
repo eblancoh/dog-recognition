af50aec64681a5940888f10480fdaef331f907d7
Enrique <ebhenriquez89@gmail.com>
2020-03-26 17:04:21 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,162 @@
+# Dog Recognition - Identificación de Perros Convolutional Neural Networks (CNNs)
+
+<p align="center">
+<img width="500"  src=./support/dog-meme.jpg>
+</p>
+Este proyecto tiene como misión hacer uso de redes convolucionales para la identificación de perros haciendo uso de redes neuronales profundas convolucionales. Las capas convolucionales actúan como extractores de características.
+
+Con la intención de ahorrar tiempo, se pretende hacer uso de modelos ya entrenados de Deep Learning para la identificación de este tipo de mascotas.
+
+Para ello, se realiza Transfer Learning y Fine-Tuning de los modelos de Oxford VGGFace a través del endopoint de TensorFlow. El código de este repositorio soporta tanto los modelos **VGG16** como **RESNET50** o **SENET50**:
+
+```python
+from keras_vggface.vggface import VGGFace
+
+# Based on VGG16 architecture -> old paper(2015)
+vggface = VGGFace(model='vgg16') # or VGGFace() as default
+
+# Based on RESNET50 architecture -> new paper(2017)
+vggface = VGGFace(model='resnet50')
+
+# Based on SENET50 architecture -> new paper(2017)
+vggface = VGGFace(model='senet50')
+```
+Tanto en su versión:
+ 
+```python 
+include_top=False
+``` 
+que permiten quedarte sólo con la parte convolucional de la red para hacer un stacking superior de un clasificador a placer
+
+Como en la versión 
+```python 
+include_top=True
+```
+la cual incluye la parte de clasificación original con todas las capas densas, lo que lo hace más pesado.
+
+Los pesos para los modelos se pueden obtener en el [siguiente enlace](https://github.com/eblancoh/cattle-recognition/releases/tag/v0.1-downloads).
+
+## Setup del entorno
+
+Se recomienda hacer uso de Anaconda para la creación de un entorno virtual.
+
+```bash 
+$ conda create --name <env> --file requirements.txt
+```
+
+## Ejemplo de uso
+
+### 1. Data Cleaning (opcional)
+
+Para evitar la baja varianza entre imágenes se emplea la medida del índice de similitud estructural (SSIM) para medir la similitud entre fotografías. Esto ayuda a evitar datos muy similares (casi idénticos en las particiones de datos de validación y entrenamiento.
+
+En los subdirectorios anidados de `./dataset` se checkea una imagen contra todas las demás y se van eliminando aquellas que sean similares por encima de un valor de similitud (entre `0` y `1`) indicado por el usuario.
+
+```bash
+$ python ssim.py --dir "dir/to/images" --threshold 0.95
+```
+
+### 2. Entrenamiento
+
+Ejemplo del entrenamiento de un dataset de imágenes:
+```bash
+$ python training.py --granja test --model resnet50 --epochs 20 --batch_size 30
+```
+La rutina realiza el entrenamiento, guarda el mejor checkpoint y devuelve un reporte de clasificación sobre el test dataset.
+
+#### 2.1. Customización de arquitectura para Transfer Learning
+El script `training.py` se lanza tal y como se muestra arriba. Este script entrena una Red Neuronal convolucional que puede ser `vgg16`, `resnet50` o `senet50` y que acaba en un clasificador que, por defecto, tiene la siguiente implementación `Sequential` de Keras:
+
+```python
+self.x = self.last_layer
+self.x = Dense(self.hidden_dim_1, activation='relu', name='fc1')(self.x)
+self.x = Dense(self.hidden_dim_2, activation='relu', name='fc2')(self.x)
+self.x = Dense(self.hidden_dim_3, activation='relu', name='fc3')(self.x)
+self.out = Dense(self.nb_class, activation='softmax', name='out')(self.x)
+```
+Clasificador de capas densas totalmente conectadas que se meten tras la capa `flatten` de los modelos convolucionales.
+
+`TODO:` ver si la regularización por Dropout mejora la performance de los modelos.
+
+#### 2.2. Congelación de capas para Fine-Tuning
+Normalmente, para Transfer Learning y Fine-Tuning de modelos con dataset pequeños, lo que se hace es congelar la arquitectira transferida y entrenar sólamente el clasificador customizado por nosotros. El número de capas a definir como entrenables se especifica en la función `main()` en la línea `277`:
+* `nb_freeze = None` indica que no se congela ninguna capa. Todas las capas son entrenables.
+* `nb_freeze = 10` indica que se congelan las 10 primeras capas. Las restantes son entrenables por defecto.
+* `nb_freeze = -4` indica que se congelan todas menos las 4 últimas capas. Las restantes son entrenables por defecto.
+
+#### 2.3. Dataset
+El dataset sobre el que se desea entrenar debe situarse en la carpeta `./dataset`. Para cada clase, se deben agrupar todas las imágenes en subdirectorios. 
+
+Los batches de entrenamiento, validación, así como el núemro de clases a predecir y, por lo tanto, la arquitectura de salida del modelo, están definidas tanto por el generador `ImageDataGenerator()` como por la función `flow_from_directory()`.
+
+Sobre el dataset disponible se hace data augmentation:
+* **Rotación aleatoria** de cada imagen de hasta `15 grados`;
+* **Desplazamiento en altura y anchura** de hasta el `5%` de la dimensión de la imagen;
+* **Horizontal flipping**.
+
+#### 2.4. Logueo del entrenamiento
+
+Para el entrenamiento se han definido dos callbacks: 
+* **EarlyStopping** para evitar overfitting o alargar innecesariamente el tiempo de entrenamiento. 
+* **TensorBoard Callback** que permite logar precisión y funciones de pérdida para su visualización en browser de las curvas de aprendizaje y del grafo creado y entrenado.
+
+```bash
+$ cd logs/folder_tbd/
+$ tensorboard --logdir=./ --port 6006
+```
+
+De manera que con sólo ir a tu navegador a `http://localhost:6006/` se puede visualizar cómo ha transcurrido el entrenamiento. Ver el [siguiente artículo](https://itnext.io/how-to-use-tensorboard-5d82f8654496) para aclarar dudas.
+
+### 3. Testeo
+De cara al testeo de un modelo ya entrenado con una imagen de muestra, se ejecuta el script `testing.py`:
+```bash
+$ python testing.py --granja test  --img "path/to/img"
+```
+
+Esta rutina devuelve las predicciones de una imagen en base a todas las clases soportadas por el modelo. La rutina devuelve:
+
+```bash
+$ python testing.py --granja test  --img "path/to/img"
+{
+    "class 0": score 0,
+    "class 1": score 1,
+    ...
+    "class N": score N
+}
+```
+
+### 4. Grad-CAM Checking
+
+Un inconveniente frecuentemente citado del uso de redes neuronales es que entender exactamente lo que están modelando es muy difícil. Esto se complica aún más utilizando redes profundas. Sin embargo, esto ha comenzado a atraer una gran cantidad de interés de investigación, especialmente para las CNNs para garantizar que la "atención" de la red se centre en las características reales y discriminativas del propio animal, en lugar de otras partes de la imagen que puedan contener información discriminatoria (por ejemplo, una etiqueta de clase, una marca de tiempo impresa en la imagen o un borde sin interés).
+
+Para comprobar que nuestro modelo se centra en partes interesantes de la imagen, se puede elegir una imagen de prueba y comprobar qué regiones son de interés para la red neuronal, se puede ejecutar:
+
+```bash
+$ python grad_CAM.py --granja test --model resnet50 --img "path/to/img"
+```
+Lo cual te devuelve un mapa de calor sobre la imagen de las regiones de interés.
+
+## Enlaces de Soporte e Interés:
+
+* [Keras Framework](www.keras.io)
+* [Oxford VGGFace Website](http://www.robots.ox.ac.uk/~vgg/software/vgg_face/)
+* [Arkhi et al.: Deep Face Recognition](http://www.robots.ox.ac.uk/~vgg/publications/2015/Parkhi15/parkhi15.pdf)
+* [Towards on-farm pig face recognition using convolutional neural networks](https://www.sciencedirect.com/science/article/pii/S0166361517304992?via%3Dihub#fig0025)
+* [VGGFace implementation with Keras Framework](https://github.com/rcmalli/keras-vggface)
+* [Deep Learning For Beginners Using Transfer Learning In Keras](https://towardsdatascience.com/keras-transfer-learning-for-beginners-6c9b8b7143e)
+* [Transfer learning from pre-trained models](https://towardsdatascience.com/transfer-learning-from-pre-trained-models-f2393f124751)
+* [Transfer Learning using Keras](https://medium.com/@14prakash/transfer-learning-using-keras-d804b2e04ef8)
+* [Tutorial on using Keras `flow_from_directory()` and generators](https://medium.com/@vijayabhaskar96/tutorial-image-classification-with-keras-flow-from-directory-and-generators-95f75ebe5720)
+* [Transfer learning and Image classification using Keras on Kaggle kernels](https://towardsdatascience.com/transfer-learning-and-image-classification-using-keras-on-kaggle-kernels-c76d3b030649)
+* [A Comprehensive Hands-on Guide to Transfer Learning with Real-World Applications in Deep Learning](https://towardsdatascience.com/a-comprehensive-hands-on-guide-to-transfer-learning-with-real-world-applications-in-deep-learning-212bf3b2f27a)
+* [Gradient-weighted Class Activation Mapping - Grad-CAM](https://medium.com/@mohamedchetoui/grad-cam-gradient-weighted-class-activation-mapping-ffd72742243a)
+* [Keras implementation of GradCAM](https://github.com/eclique/keras-gradcam/blob/master/gradcam_vgg.ipynb)
+* [Grad-CAM with keras-vis](https://fairyonice.github.io/Grad-CAM-with-keras-vis.html)
+* [A Python module for computing the Structural Similarity Image Metric (SSIM)](https://github.com/jterrace/pyssim)
+
+# Licencia
+This is free and unencumbered software released into the public domain. Anyone is free to copy, modify, publish, use, compile, sell, or distribute this software, either in source code form or as a compiled binary, for any purpose, commercial or non-commercial, and by any means.
+
+In jurisdictions that recognize copyright laws, the author or authors of this software dedicate any and all copyright interest in the software to the public domain. We make this dedication for the benefit of the public at large and to the detriment of our heirs and successors. We intend this dedication to be an overt act of relinquishment in perpetuity of all present and future rights to this software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ training.py
@@ -0,0 +1,366 @@
+
+from keras_vggface.vggface import VGGFace
+from keras.engine import Model
+from keras import models, layers
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Input
+from keras.applications.vgg16 import preprocess_input
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import SGD
+from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
+from sklearn.metrics import classification_report, confusion_matrix, plot_confusion_matrix
+from datetime import datetime
+from PIL import Image
+import numpy as np
+import argparse
+import json
+import os
+from keras import backend as K
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def recall(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+def precision(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+def f1(y_true, y_pred):
+    prec = precision(y_true, y_pred)
+    rec = recall(y_true, y_pred)
+    return 2*((prec*rec)/(prec+rec+K.epsilon()))
+
+
+class CustomVGGModel(object):
+    def __init__(self, model, height, width, channels, nb_class, nb_freeze,**kwargs):
+        self.model = model
+        self.height = height
+        self.width = width
+        self.channels = channels
+
+        self.nb_class = nb_class
+        self.nb_freeze = nb_freeze
+
+        self.hidden_dim_1 = 512
+        self.hidden_dim_2 = 256
+        self.hidden_dim_3 = 128
+        # self.hidden_dim_4 = 128
+    
+    def model_build(self):
+
+        # Carga del modelo por defecto
+        """
+        self.vggface = VGGFace(include_top=False, 
+                                 model=self.model, 
+                                 input_shape=(
+                                     self.height, 
+                                     self.width, 
+                                     self.channels
+                                     )
+                                )
+        """
+        self.vggface = VGGFace(include_top=True, 
+                                 model=self.model, 
+                                 input_shape=(
+                                     self.height, 
+                                     self.width, 
+                                     self.channels
+                                     )
+                                )
+        # Añadimos capas de clasificación a la red convolucional
+        """
+        if self.model == 'vgg16':
+            self.last_layer = self.vggface.get_layer('pool5').output
+        elif self.model == 'resnet50':
+            self.last_layer = self.vggface.get_layer('avg_pool').output
+        elif self.model == 'senet50':
+            self.last_layer = self.vggface.get_layer('avg_pool').output
+        """
+        if self.model == 'vgg16':
+            self.last_layer = self.vggface.get_layer('flatten').output
+        elif self.model == 'resnet50':
+            self.last_layer = self.vggface.get_layer('flatten_1').output
+        elif self.model == 'senet50':
+            self.last_layer = self.vggface.get_layer('flatten_1').output
+        """
+        self.x = Flatten(name='flatten')(self.last_layer)
+        """
+        self.x = self.last_layer
+        self.x = Dense(self.hidden_dim_1, activation='relu', name='fc1')(self.x)
+        self.x = Dense(self.hidden_dim_2, activation='relu', name='fc2')(self.x)
+        self.x = Dense(self.hidden_dim_3, activation='relu', name='fc3')(self.x)
+        # self.x = Dense(self.hidden_dim_3, activation='relu', name='fc4')(self.x)
+        self.out = Dense(self.nb_class, activation='softmax', name='out')(self.x)
+        # Now a custom model has been created based on our architecture
+        # supported in Visual Geometry Group
+        self.custom_model = Model(inputs=self.vggface.input, outputs=self.out)
+        
+        return self.custom_model
+    
+    def set_trainable(self):
+        # If we want to set the first self.nb_freeze layers of the network to be frozen
+        # the rest should remain trainable. 
+        # Our dataset is small and very different from human faces!
+        # It will be hard to find a balance between the number of layers to train and freeze. 
+        # If you go to deep your model can overfit, if you stay in the shallow end 
+        # of your model you won’t learn anything useful.
+        if self.nb_freeze is None:
+            # Mantenemos como entrenables todas las capas
+            for layer in self.custom_model.layers[:]:
+                layer.trainable=True
+        else:
+            if self.nb_freeze != 0:
+                # Entendemos que un número positivo indica que 
+                # queremos congelar las self.nb_freeze primeras capas
+                # Entendemos que un número negativo indica que 
+                # queremos congelar todas las capas salvo las self.nb_freeze últimas capas
+                for layer in self.custom_model.layers[:self.nb_freeze]:
+                    layer.trainable=False
+            elif self.nb_freeze == 0:
+                # Entendemos que un 0 indica que no queremos congelar nada
+                for layer in self.custom_model.layers[:]:
+                    layer.trainable=True
+
+    def custom_model_summary(self):
+        print(self.custom_model.summary())
+        print('Status de las diferentes capas del modelo:')
+        for i,layer in enumerate(self.custom_model.layers):
+            print(i, layer.name, layer.trainable)
+    
+    def compiler(self, optimizer, loss, metrics):
+        self.custom_model.compile(optimizer=optimizer,
+                           loss=loss,
+                           metrics=metrics)
+        return self.custom_model
+
+
+class Generator(object):
+    def __init__(self, height, width, bs, dir, **kwargs):
+        self.height = height
+        self.width = width
+        self.batch_size = bs
+        self.dir = dir # 'path-to-the-main-data-folder'
+
+        self.generator=ImageDataGenerator(preprocessing_function=preprocess_input,
+                            #featurewise_std_normalization = True,
+                            rotation_range=15,
+                            zoom_range=[0.9, 1.25],
+                            brightness_range=[0.8, 1.2],
+                            width_shift_range=self.width*0.005,
+                            height_shift_range=self.height*0.005,
+                            #shear_range=0.01,
+                            horizontal_flip=True,
+                            validation_split=0.2
+                            ) 
+
+    def dataset_generator(self):
+
+        train_generator=self.generator.flow_from_directory(self.dir,
+                                                target_size=(self.height,self.width),
+                                                color_mode='rgb',
+                                                batch_size=self.batch_size,
+                                                class_mode='categorical',
+                                                shuffle=True,
+                                                subset='training')
+
+        test_generator=self.generator.flow_from_directory(self.dir,
+                                                 target_size=(self.height,self.width),
+                                                 color_mode='rgb',
+                                                 batch_size=self.batch_size,
+                                                 class_mode='categorical',
+                                                 shuffle=False,
+                                                 subset='validation')
+
+        step_size_train=train_generator.n//train_generator.batch_size
+        step_size_test=test_generator.n//test_generator.batch_size
+
+        return train_generator, test_generator, step_size_train, step_size_test
+
+
+class FitGenerator(object):
+    def __init__(self, model, train_generator, test_generator, step_size_train, step_size_test, epochs,**kwargs):
+        self.model = model
+        self.train_generator = train_generator
+        self.test_generator = test_generator
+        self.step_size_train = step_size_train
+        self.step_size_test = step_size_test
+        self.epochs = epochs
+        self.epochs_to_wait = 5
+
+    def train(self, farm):
+
+        CheckpointsMkdir(farm).check()
+        LogsMkdir().check()
+    
+        early_stopping_callback = EarlyStopping(monitor='val_loss', 
+                                                patience=self.epochs_to_wait)
+        checkpoint_callback = ModelCheckpoint(filepath=os.path.join('./checkpoints', farm, 'chckpt.best.h5'), 
+                                              monitor='val_loss', 
+                                              verbose=1, 
+                                              save_best_only=True, 
+                                              mode='min')
+        tmstmp = datetime.now().strftime("%Y%m%d-%H%M%S")
+
+        logdir = './logs/' + tmstmp
+        tensorboard_callback = TensorBoard(log_dir=logdir,
+                                          histogram_freq=0, # It is general issue with keras/tboard that you cannot get histograms with a validation_generator
+                                          write_graph=True,
+                                          write_images=True,
+                                          update_freq='batch'
+                                          )
+
+        self.model.fit_generator(generator=self.train_generator, \
+                               steps_per_epoch=self.step_size_train, \
+                               validation_data=self.test_generator, \
+                               validation_steps=self.step_size_test, \
+                               epochs=self.epochs, \
+                               verbose=1, \
+                               callbacks=[
+                                   early_stopping_callback, 
+                                   checkpoint_callback,
+                                   tensorboard_callback
+                                   ])
+    
+    def confusion(self, farm):
+        # Reiniciamos el test_generator.
+        self.test_generator.reset()
+        # class_pred = self.model.predict_generator(self.test_generator, \
+        #                                           self.step_size_test + 1)
+        class_pred = self.model.predict_generator(self.test_generator, verbose=1)
+        class_pred = np.argmax(class_pred, axis=1)
+        # Obtenemos los nombres de las etiquetas como una lista desde json
+        with open(os.path.join('./checkpoints', farm, 'labels.json')) as json_file:
+            json_classes = json.load(json_file)
+        classes = self.test_generator.classes[self.test_generator.index_array]
+        target_names = list(json_classes.keys())
+        print('Classification Report')
+        # Reporte de clasificación
+        print(classification_report(self.test_generator.classes, 
+                                    class_pred, 
+                                    target_names=target_names,
+                                    digits=5))
+        cm = pd.DataFrame(confusion_matrix(classes, class_pred), \
+                                           index = target_names, \
+                                           columns = target_names)
+        sns.heatmap(cm, annot=True)
+        #Guardamos la matriz de confusión en jpg
+        plt.savefig('confusion_matrix_' + datetime.now().strftime("%Y%m%d-%H%M%S") + '.jpg')
+        
+
+class CheckpointsMkdir(object):
+    def __init__(self, farm):
+        self.farm = farm
+        self.bool = os.path.isdir(os.path.join('./checkpoints', self.farm))
+    def check(self):
+        if not self.bool:
+            os.mkdir(os.path.join('./checkpoints', self.farm))
+            #print('Checkpoints folder created. \n Skipping creation.')
+        else:
+            pass
+            #print('Checkpoints folder already exists. \n Skipping creation.')
+
+
+class LogsMkdir(object):
+    def __init__(self):
+        self.bool = os.path.isdir('./logs')
+    def check(self):
+        if not self.bool:
+            os.mkdir('./logs')
+            #print('Logging folder created.')
+        else:
+            pass
+            #print('Logging folder already exists. \n Skipping creation.')
+
+
+class InputShape(object):
+    def __init__(self, image, **kwargs):
+        self.image = image
+    def _shape(self):
+        return Image.open(self.image).size # returns (width, height) tuple
+
+class ClassParser(object):
+    def __init__(self, input, farm, dir, **kwargs):
+        self.input = input
+        self.farm = farm
+        self.dir = dir
+    def save(self):
+        CheckpointsMkdir(self.farm).check()
+        filepath = os.path.join(self.dir, self.farm) + '\\labels.json'
+        with open(filepath, 'w') as fp:
+            json.dump(self.input, 
+                      fp, 
+                      sort_keys=True, 
+                      indent=4)
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        description="Convolutional Neural Network training routine for cattle recognition",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        fromfile_prefix_chars='@')
+    parser.add_argument('--granja', '-ng', type=str, default='nome', help='Nome da granja')
+    parser.add_argument('--model', '-m', type=str, default='resnet50', help='Modelo a ser usado para treinamento. Valores suportados: vgg16, resnet50 ou senet50. Valor padrão: vgg16')
+    parser.add_argument('--epochs', '-ep', type=int, default=20, help='Número de épocas para treinar o modelo')
+    parser.add_argument('--batch_size', '-bs', type=int, default=30, help='Tamanho do lote das amostras para treinar o modelo')
+
+    args = parser.parse_args()
+
+    if not args.granja:
+        parser.print_help()
+        raise ValueError('Você deve proporcionar o nome da granja após --granja')
+
+    train_generator, \
+    test_generator, \
+    step_size_train, \
+    step_size_test = Generator(height=224, 
+                               width=224, 
+                               bs=args.batch_size, 
+                               dir='./dataset').dataset_generator()
+
+    deep_model = CustomVGGModel(model=args.model, 
+                                height=224, 
+                                width=224, 
+                                channels=3, 
+                                nb_class=train_generator.class_indices.__len__(), # That way we get the number of classes automatically
+                                nb_freeze=-4)
+    
+    # Guardamos las clases, las vamos a necesitar para testeo
+
+    ClassParser(input=train_generator.class_indices,
+                farm=args.granja,
+                dir='./checkpoints').save()
+    
+    deep_model.model_build()
+    deep_model.set_trainable()
+    deep_model.custom_model_summary()
+
+    compiled_deep_model= deep_model.compiler(optimizer='Adam', 
+                                             loss='categorical_crossentropy', 
+                                             metrics=['accuracy', precision, recall, f1])
+
+    FitGenerator(model=compiled_deep_model, 
+                 train_generator=train_generator, 
+                 test_generator=test_generator, 
+                 step_size_train=step_size_train, 
+                 step_size_test=step_size_test, 
+                 epochs=args.epochs).train(args.granja)
+    
+    FitGenerator(model=compiled_deep_model, 
+                 train_generator=train_generator, 
+                 test_generator=test_generator, 
+                 step_size_train=step_size_train, 
+                 step_size_test=step_size_test, 
+                 epochs=args.epochs).confusion(args.granja)
+
+if __name__ == '__main__':
+    main()
+
+# TODO: gridsearch
